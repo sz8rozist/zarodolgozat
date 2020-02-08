bbdb2786ff6d352411f54340877e3885d9a9c6e8
sz8rozist <p.ityu1998@gmail.com>
2020-02-08 07:13:47 +0100

    etkezes class, tulajdonságok, konstruktor setter getter

--- byb/Modell/Etkezes.cs
+++ byb/Modell/Etkezes.cs
@@ -10,7 +10,20 @@ namespace byb.Modell
     {
         private int etkezesid;
         private string idopont;
-        private string etel;
-        private string fname;
+        private string etelid;
+        private string fid;
+
+        public Etkezes(int etkezesid, string idopont, string etelid, string fid)
+        {
+            this.etkezesid = etkezesid;
+            this.idopont = idopont;
+            this.etelid = etelid;
+            this.fid = fid;
+        }
+
+        public int Etkezesid { get => etkezesid; set => etkezesid = value; }
+        public string Idopont { get => idopont; set => idopont = value; }
+        public string Etelid { get => etelid; set => etelid = value; }
+        public string Fid { get => fid; set => fid = value; }
     }
 }
